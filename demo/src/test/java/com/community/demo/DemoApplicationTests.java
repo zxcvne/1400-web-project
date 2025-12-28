da7fa8190e4a863b8833e12cd9e0981ce6dff1d2
hyunseo2512 <nhs8421@gmail.com>
2025-12-28 14:47:19 +0900

    update

---
+++ demo/src/test/java/com/community/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
